13a5d04e1597aa063fbe10faa7b19cb342b317eb
Ziplogcom <ziplogcom@outlook.com>
2025-08-15 21:20:27 -0600

    modified:   main.lua

--- main.lua
+++ main.lua
@@ -10,3 +10,9 @@ os.sleep(0.1)
 StatusText.setText("Booting into Headless Mode...")
 os.sleep(1)
 
+-- Begin 3D System Test
+StatusText.setText("Loading 3D System...")
+local canvas3 = driver.canvas3d
+local testBox = canvas3.addBox(-5, -5, -5[, 5, 5, 5][, 0x220000ff])
+os.sleep(2)
+testBox.delete()
